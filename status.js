81fd7f6bf18921901d05534e49ab3ec50248c44d
VoxDaniel <lepinotj@gmail.com>
2026-07-27 07:53:16 +0700

    Update 3 files: status.js, weekly.jpg, weekly2.jpg

--- status.js
+++ status.js
@@ -2,6 +2,6 @@ document.write(`
 
 I've been lucky enough to take the KRL to Solo City a few days ago to meet up with old friends. And this Saturday, I've arrived at my second house in Munthuk, Mount Sumbing — feeling grateful.<br/><br/>
 
-And as you can see below, it's a picture of the sunset at Mount Menoreh. I've been here twice at Kopi Ingkar Janji — not bad for escaping the city bustle for a few hours.
+Below is a picture of Mount Sindoro, taken just in front of the door of my house in Mount Sumbing — quite a mouthful. 🤣 
 
 `)
