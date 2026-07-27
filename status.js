f473a096e0c9d9caffef528e2d525ee95f7e10e8
VoxDaniel <lepinotj@gmail.com>
2026-07-27 07:24:06 +0700

    Update 1 file: status.js

--- status.js
+++ status.js
@@ -1,8 +1,6 @@
 document.write(`
 
-I'm still trying my best to find ideas for more songs to fill the 3‑hour playlist quota. Currently, I have 43 songs and still need about 30 more minutes. <br/><br/>
-
-I've been lucky enough to take the KRL to Solo City a few days ago to meet up with old friends. And today, I've arrived at my second house in Munthuk, Mount Sumbing — feeling grateful.<br/><br/>
+I've been lucky enough to take the KRL to Solo City a few days ago to meet up with old friends. And this Saturday, I've arrived at my second house in Munthuk, Mount Sumbing — feeling grateful.<br/><br/>
 
 And as you can see below, it's a picture of the sunset at Mount Menoreh. I've been here twice at Kopi Ingkar Janji — not bad for escaping the city bustle for a few hours.
 
