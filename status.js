6b5c6589bc55378feff45895e1b288066193c89b
VoxDaniel <lepinotj@gmail.com>
2026-07-31 10:16:51 +0700

    Update 1 file: status.js

--- status.js
+++ status.js
@@ -1,6 +1,8 @@
 document.write(`
 
-I've been lucky enough to take the KRL to Solo City a few days ago to meet up with old friends. And this Saturday, I've arrived at my second house in Munthuk, Mount Sumbing — feeling grateful.<br/><br/>
+The weather over the past two years in Jogja has been uncomfortable for many people, especially singers. It makes us wonder: "Has nature really changed like this, or is this just a phase?"
+
+---
 
 Below is a picture of Mount Sindoro, taken just in front of the door of my house in Mount Sumbing — quite a mouthful. 🤣 
 
